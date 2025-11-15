042f09f457955f9df6549a136e32a3c0f73a0a72
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2025-11-15 18:58:03 -0300

    Adicionando anotações

--- src/main/java/io/github/henriquemcc/dtidigital/testepratico/model/Pedido.java
+++ src/main/java/io/github/henriquemcc/dtidigital/testepratico/model/Pedido.java
@@ -1,15 +1,14 @@
 package io.github.henriquemcc.dtidigital.testepratico.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Pedido {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     public long id;
     public float peso;
+    @Enumerated(EnumType.STRING)
     public Prioridade prioridade;
+    @Embedded
     public Coordenada coordenada;
 }
