312e62c18ae83d3a294326156f59085930c51bf8
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2025-11-15 18:38:33 -0300

    Implementando PedidoController

---
+++ src/main/java/io/github/henriquemcc/dtidigital/testepratico/controller/PedidoController.java
@@ -0,0 +1,37 @@
+package io.github.henriquemcc.dtidigital.testepratico.controller;
+
+import io.github.henriquemcc.dtidigital.testepratico.model.Pedido;
+import io.github.henriquemcc.dtidigital.testepratico.service.PedidoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pedidos")
+public class PedidoController {
+    private final PedidoService pedidoService;
+
+    public PedidoController(PedidoService pedidoService){
+        this.pedidoService = pedidoService;
+    }
+
+    @GetMapping
+    public List<Pedido> listar() {
+        return pedidoService.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Pedido buscarPorId(@PathVariable long id) {
+        return pedidoService.buscarPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public Pedido atualizar(@RequestBody Pedido pedido, @PathVariable long id) {
+        return pedidoService.atualizar(pedido, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable long id) {
+        pedidoService.deletar(id);
+    }
+}
