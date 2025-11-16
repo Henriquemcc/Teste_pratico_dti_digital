92a6308747fad9575f8de722d4cb20b055e91136
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2025-11-16 05:56:17 -0300

    Adicionando anotações

--- src/main/java/io/github/henriquemcc/dtidigital/testepratico/model/Coordenada.java
+++ src/main/java/io/github/henriquemcc/dtidigital/testepratico/model/Coordenada.java
@@ -1,5 +1,8 @@
 package io.github.henriquemcc.dtidigital.testepratico.model;
 
+import jakarta.persistence.Embeddable;
+
+@Embeddable
 public class Coordenada {
     public double x;
     public double y;
--- src/main/java/io/github/henriquemcc/dtidigital/testepratico/model/Rota.java
+++ src/main/java/io/github/henriquemcc/dtidigital/testepratico/model/Rota.java
@@ -1,6 +1,20 @@
 package io.github.henriquemcc.dtidigital.testepratico.model;
 
+import jakarta.persistence.*;
+
+@Embeddable
 public class Rota {
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="x", column = @Column(name = "origem_x")),
+            @AttributeOverride(name="y", column = @Column(name = "origem_y"))
+    })
     public Coordenada origem;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="x", column = @Column(name = "destino_x")),
+            @AttributeOverride(name="y", column = @Column(name = "destino_y"))
+    })
     public Coordenada destino;
 }
