05e65c9c6edb5f6df8af302435c3dab5a7f65784
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2025-11-16 08:13:07 -0300

    Adicionando RotaService

---
+++ src/main/java/io/github/henriquemcc/dtidigital/testepratico/repository/RotaRepository.java
@@ -0,0 +1,8 @@
+package io.github.henriquemcc.dtidigital.testepratico.repository;
+
+import io.github.henriquemcc.dtidigital.testepratico.model.Rota;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RotaRepository extends JpaRepository<Rota, Long> {
+
+}
---
+++ src/main/java/io/github/henriquemcc/dtidigital/testepratico/service/RotaService.java
@@ -0,0 +1,44 @@
+package io.github.henriquemcc.dtidigital.testepratico.service;
+
+import io.github.henriquemcc.dtidigital.testepratico.exception.NotFoundException;
+import io.github.henriquemcc.dtidigital.testepratico.model.Rota;
+import io.github.henriquemcc.dtidigital.testepratico.repository.RotaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RotaService {
+    private final RotaRepository rotaRepository;
+    private final String notFoundMessage = "Rota não encontrada";
+
+    public RotaService(RotaRepository rotaRepository){
+        this.rotaRepository = rotaRepository;
+    }
+
+    public List<Rota> listar() {
+        return rotaRepository.findAll();
+    }
+
+    public Rota buscarPorId(long id) {
+        return rotaRepository.findById(id).orElseThrow(() -> new NotFoundException(notFoundMessage));
+    }
+
+    public Rota cadastrar(Rota rota) {
+        rotaRepository.save(rota);
+        return rota;
+    }
+
+    public Rota atualizar(Rota rota, long idRota) {
+        Rota rotaAnterior = rotaRepository.findById(idRota).orElseThrow(() -> new NotFoundException(notFoundMessage));
+        rotaAnterior.destino = rota.destino;
+        rotaAnterior.origem = rota.origem;
+        return rotaAnterior;
+    }
+
+    public void deletar(long id) {
+        rotaRepository.deleteById(id);
+    }
+
+
+}
