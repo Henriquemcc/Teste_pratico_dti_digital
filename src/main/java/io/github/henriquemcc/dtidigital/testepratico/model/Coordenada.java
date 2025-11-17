baa3052b0e4e0bb3df81d06b72aa4dc003baa5bb
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2025-11-17 09:43:27 -0300

    Adicionando construtores

--- src/main/java/io/github/henriquemcc/dtidigital/testepratico/model/Coordenada.java
+++ src/main/java/io/github/henriquemcc/dtidigital/testepratico/model/Coordenada.java
@@ -6,4 +6,13 @@ import jakarta.persistence.Embeddable;
 public class Coordenada {
     public double x;
     public double y;
+
+    public Coordenada(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Coordenada() {
+
+    }
 }
