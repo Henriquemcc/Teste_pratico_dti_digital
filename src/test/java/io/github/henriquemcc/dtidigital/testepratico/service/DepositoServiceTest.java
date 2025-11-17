c251755eff0cde7cee92729e626e65810e3c83aa
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2025-11-17 12:20:38 -0300

    Adicionando caso de teste

--- src/test/java/io/github/henriquemcc/dtidigital/testepratico/service/DepositoServiceTest.java
+++ src/test/java/io/github/henriquemcc/dtidigital/testepratico/service/DepositoServiceTest.java
@@ -1,5 +1,6 @@
 package io.github.henriquemcc.dtidigital.testepratico.service;
 
+import io.github.henriquemcc.dtidigital.testepratico.exception.NotFoundException;
 import io.github.henriquemcc.dtidigital.testepratico.model.Deposito;
 import io.github.henriquemcc.dtidigital.testepratico.model.DepositoTest;
 import io.github.henriquemcc.dtidigital.testepratico.repository.DepositoRepository;
@@ -10,8 +11,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -29,5 +33,15 @@ public class DepositoServiceTest {
         when(depositoRepository.findAll()).thenReturn(depositos);
         List<Deposito> resultado = depositoService.listar();
         assertEquals(resultado, depositos);
+    }
+
+    @Test
+    public void deveListarNotFoundExceptionQuandoDepositoNaoForAchado() {
+        long idInexistente = 999L;
+        when(depositoRepository.findById(idInexistente)).thenReturn(Optional.empty());
+
+        NotFoundException thrown = assertThrows(NotFoundException.class, () -> depositoService.buscarPorId(idInexistente));
+
+        assertEquals("Depósito não encontrado", thrown.getMessage());
     }
 }
